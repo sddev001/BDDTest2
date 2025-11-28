59a2b4e6a7cb15ed336e1d3d7729b3da58881a9c
shibu <shibu@sdd>
2025-11-27 21:54:32 -0600

    Commit4

--- BDDTest1/src/test/java/runner/RunCuke.java
+++ BDDTest1/src/test/java/runner/RunCuke.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/features", glue= {"steps"},
-plugin = {"html:target/cucumber-reports/cucumber-html-report.html",	
+plugin = {"pretty", "html:target/cucumber-reports/cucumber-html-report.html",	
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class RunCuke extends AbstractTestNGCucumberTests{
 }
--- BDDTest1/src/test/resources/features/GoogleSearch.feature
+++ BDDTest1/src/test/resources/features/GoogleSearch.feature
@@ -1,6 +1,6 @@
 Feature: Searching Google
 
-  Scenario: Searching Google
+  Scenario: Searching Google2
     Given user navigates to Google website
     When user validate Google title
     Then user close Google browser
